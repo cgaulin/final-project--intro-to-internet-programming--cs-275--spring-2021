9bea5acd8878647519463650ab90ad23998c2da6
cgaulin <cgaulin@hartford.edu>
2021-05-11 21:25:51 -0400

    If looper variable is even, continue

--- app/js/app.js
+++ app/js/app.js
@@ -25,6 +25,9 @@ window.onload = () => {
 
         for(i = 0; i < val * 2 - 1; i++) {
           looper = i < val ? i : val * 2 - i - 2;
+          if(looper % 2 === 0){
+              continue;
+          }
     }
 
     }
